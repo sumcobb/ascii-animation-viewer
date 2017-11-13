1aebb3ea9f4924dd3f974d7e2118eb81f41b6234
turner36 <turner36@students.wwu.edu>
2017-11-12 22:40:54 -0800

    fixed typo, added click for start/stop button

---
+++ ascii.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var startBtn = $("#startButton");
+var stopBtn = $("#stopButton");
+
+
+$(startBtn).on('click', function () {
+    alert("start");
+});
+
+$(stopBtn).on('click', function () {
+    alert("stop");
+});
